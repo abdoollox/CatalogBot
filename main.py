fa7d864022497d7a125a843e7e6df9702ae1e398
abdoollox <abdoollox@gmail.com>
2026-02-25 15:34:45 +0500

    main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1003826689337"))
 CHANNEL_URL = "https://t.me/garripotter_cinema"
-WEBAPP_URL = "https://sening-domen.uz"
+WEBAPP_URL = "https://abdoollox.github.io/WebApp/"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
